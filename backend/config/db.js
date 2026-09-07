db411753cb0613d5ed2205200facaa169331e725
Oumer Adem <oumerahmed0@gmail.com>
2026-09-06 21:08:28 -0700

    Remove hardcoded MongoDB password from db.js (read from env only)

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dessie:***REMOVED***@cluster0.qxqlnqo.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("DB Connected"));
 
 }
