792bd90160fe18e8e3ee8277c366333afd1ee6aa
Oumer Adem <oumerahmed0@gmail.com>
2026-09-06 19:13:48 -0700

    Fix MongoDB connection: read MONGODB_URI from env instead of hardcoded password

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://dessie:***REMOVED***@cluster0.qxqlnqo.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://dessie:***REMOVED***@cluster0.qxqlnqo.mongodb.net/food-del').then(()=>console.log("DB Connected"));
 
 }
